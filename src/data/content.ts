14a0f62b1330941e66b733267171aa784ad2c7a5
David <davidluque2610@gmail.com>
2026-03-06 21:44:50 -0500

    ubications

--- src/data/content.ts
+++ src/data/content.ts
@@ -7,11 +7,11 @@ export const VALERIA_DATA = {
     madrina: "Zulema Supo Payé"
   },
   event: {
-    churchMapUrl:"https://maps.app.goo.gl/MjVk92z74g3ngfLn6",
+    churchMapUrl:"https://www.google.com/maps/place/Templo+de+la+Merced/@-15.4966088,-70.131963,108m/data=!3m1!1e3!4m14!1m7!3m6!1s0x9167f3b6c39bfcd3:0x3ef74cfa78c75e33!2sParroquia+%22Cristo+rey%22!8m2!3d-15.4976865!4d-70.1307002!16s%2Fg%2F11ty14k2tc!3m5!1s0x9167f3efa7694ea5:0x350fb775b97fb95e!8m2!3d-15.496599!4d-70.1316668!16s%2Fg%2F11c52l97s5?entry=ttu&g_ep=EgoyMDI2MDMwNC4xIKXMDSoASAFQAw%3D%3D",
     salonMapUrl:"https://maps.app.goo.gl/KwouVkNzQiB1Uqog8",
     date: "Sábado 21 de Marzo",
     fullDateISO: "2026-03-21T17:00:00",
-    church: "Misa de Salud Iglesia Cristo Rey",
+    church: "Misa de Salud Templo de la Merced",
     churchLocation: "Plaza Bolognesi",
     churchTime: "5:00 PM",
     receptionTime: "6:00 PM",
